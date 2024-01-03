0a2188add910bbabf573e812a1e8c05e20967402
박용진 <qqq4861@naver.com>
2024-01-03 16:57:20 +0900

    test2

--- tt1/src/tt1/T1.java
+++ tt1/src/tt1/T1.java
@@ -7,7 +7,7 @@ public class T1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] board = 
-			{{1,0,0,0,0}
+			{{1,1,0,0,0}
 			,{0,0,0,0,0}
 			,{0,0,1,0,0}
 			,{0,0,0,0,0}
